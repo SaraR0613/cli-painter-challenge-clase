bd98c3251eff31e770ba46905519f28ed7f88add
B12_309_11\b12s309 <yajairaivondelriocardona@gmail.com>
2024-08-06 11:36:24 -0500

    area, draw, _str_  (triangle)

--- painter/model.py
+++ painter/model.py
@@ -30,4 +30,29 @@ class circle:
 
 class triangle:
 
+    def __int__(self, point_1: point, point_2: point, point_3: point):
+        self.point_1: point = point_1
+        self.point_2: point = point_2
+        self.point_3: point = point_3
+
+    def area(self)->float:
+        area: 1/2 ()
+
+
+    def draw(self):
+        x = [self.point_1.x, self.point_2.x, self.point_3.x, self.point_1.x]
+        y = [self.point_1.y, self.point_2.y, self.point_3.y, self.point_1.y]
+        plt.fill(x, y, color='b')
+        plt.axis("scaled")
+        plt.show()
+
+    def _str_(self):
+        triangle
+        return f"with vertices at {self.point_1.x}, {self.point_1.y}, {self.point_2.x}, {self.point_2.y} and {self.point_3.x}, {self.point_3.y}"
+
+
+class rectangle:
+
     def __int__(self, ):
+
+
