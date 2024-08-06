78f542f4027b8c30faef357e2ba7ca9d7fd11bdd
B12_309_11\b12s309 <yajairaivondelriocardona@gmail.com>
2024-08-06 11:08:40 -0500

    Draw, area, _str_

--- painter/model.py
+++ painter/model.py
@@ -1,6 +1,6 @@
 # TODO: Add code here
 import math
-
+import matplotlib.pyplot as plt
 
 class point:
 
@@ -11,8 +11,20 @@ class point:
 class circle:
 
     def __int__(self, center: point, radius: float):
-        self.radius: float = 0
+        self.radius: float = radius
+        self.center: point = center
 
     def area(self)-> float:
-        Area= math.pi * self.radius
+        Area= math.pi * (self.radius)** 2
+        return (Area)
+    
+    def draw(self):
+        circle = plt.Circle((self.center.x, self.center.y), self.radius, color="r")
+        plt.gca().add_patch(circle)
+        plt.axis("scaled")
+        plt.show()
 
+    def  _str_ (self):
+        circle
+        with center at (x, y) and radius r
+        return f "{x}{y}{e}"
