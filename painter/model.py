2961b63695323989f9070e45bbf20f1d54d12e3c
B12_309_11\b12s309 <yajairaivondelriocardona@gmail.com>
2024-08-06 10:46:01 -0500

    point, circle

--- painter/model.py
+++ painter/model.py
@@ -1,1 +1,18 @@
 # TODO: Add code here
+import math
+
+
+class point:
+
+    def __int__(self, x: float, y: float):
+        self.x: float = 0
+        self.y: float = 0
+
+class circle:
+
+    def __int__(self, center: point, radius: float):
+        self.radius: float = 0
+
+    def area(self)-> float:
+        Area= math.pi * self.radius
+
